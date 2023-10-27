386abb434bca6beb8ab6b7e9ef2087f0f928b9c2
원감 <wongam@wongam-ui-MacBookAir.local>
2023-10-27 18:37:36 +0900

    Sol : PRG_42746

--- src/programmers/PRG_42746.java
+++ src/programmers/PRG_42746.java
@@ -1,6 +1,28 @@
 package programmers;
 
-public class PRG_42748 {
-    
-}
+import java.util.Arrays;
 
+class PRG_42746 {
+    public String solution(int[] numbers) {
+        String[] answer = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(answer, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        if (answer[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : answer) {
+            sb.append(s);
+        }
+
+        return sb.toString();
+
+    }
+}
