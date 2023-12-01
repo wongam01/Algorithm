4d1a3248e883fcb72ab238f908270120b1417916
원감 <wongam@wongam-ui-MacBookAir.local>
2023-12-02 02:34:17 +0900

    Sol:BOJ_10810

---
+++ src/baekjoon/BOJ_10810.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_10810 {
+    public static void main(String[] args) throws IOException {
+        // 바구니 N개 공 N개 , 공은 M번 넣음
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int[] bucket = new int[N];
+
+        for (int t = 0; t < M; t++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int i = Integer.parseInt(st.nextToken()) - 1;
+            int j = Integer.parseInt(st.nextToken()) - 1;
+            int k = Integer.parseInt(st.nextToken());
+
+            for (int y = 0; y <= j; y++) {
+                bucket[y] = k;
+            }
+        }
+        for (int number : bucket) {
+            bw.write(number + " ");
+        }
+        bw.flush();
+    }
+}
