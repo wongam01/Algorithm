479f2faa62b4bf82ec58fdbb1bec5b3b7745275b
원감 <wongam@kyonggi.ac.kr>
2023-12-11 21:36:44 +0900

    PRG_120845

---
+++ 프로그래머스/programmers/PRG_120845.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class PRG_120845 {
+    public int solution(int[] box, int n) {
+        int answer = 1;
+        int[] array = new int[3];
+        for (int i = 0; i < 3; i++) {
+            array[i] = box[i] / n;
+            answer = answer * array[i];
+        }
+        return answer;
+    }
+}
